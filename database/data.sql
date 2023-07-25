c1db35db85ab3ed15db9342e17ad02300436e6e7
sinadaryaei <sinatangssiran@gmail.com>
2023-07-25 23:43:26 +0000

    main

--- database/data.sql
+++ database/data.sql
@@ -3,9 +3,9 @@
 
 -- EXAMPLE:
 
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+ insert into "todos"
+   ("task", "isCompleted")
+   values
+     ('Learn to code', false),
+     ('Build projects', false),
+     ('Get a job', false);
