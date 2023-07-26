ae6ef4694e41362913aa5028e11bf7bf00fbcca8
sinadaryaei <sinatangssiran@gmail.com>
2023-07-26 22:19:31 +0000

    game-interface-and-logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,75 @@
-import React, { useState } from 'react';
-import TicTacToe from './TicTacToe';
-import './App.css';
-import calculateWinner from './gameUtils';
+// import React, { useState, useEffect } from 'react';
+// import TicTacToe from './TicTacToe';
+// import './App.css';
+// import calculateWinner from './gameUtils';
+// import { io } from 'socket.io-client';
 
-const socket = new window.WebSocket('ws://server:8000');
-console.log('WebSocket server listening on port 8000');
-function App() {
-  const initialBoard = Array(9).fill(null);
-  const [board, setBoard] = useState(initialBoard);
-  const [xIsNext, setXIsNext] = useState(true);
-  const [winner, setWinner] = useState(null);
+// function App() {
+//   const initialBoard = Array(9).fill(null);
+//   const [board, setBoard] = useState(initialBoard);
+//   const [xIsNext, setXIsNext] = useState(true);
+//   const [winner, setWinner] = useState(null);
 
-  const handleClick = (i) => {
-    if (board[i] || winner) {
-      return;
-    }
+//   const socket = io('http://localhost:8000'); // Connect to the Socket.IO server
 
-    const newBoard = board.slice();
-    newBoard[i] = xIsNext ? 'X' : 'O';
-    setBoard(newBoard);
-    setXIsNext(!xIsNext);
+//   useEffect(() => {
+//     socket.on('message', (data) => {
+//       if (data.isGameOver) {
+//         // Handle game over
+//       } else if (data.board) {
+//         // Update the board state
+//         setBoard(data.board);
+//       }
+//     });
 
-    const newWinner = calculateWinner(newBoard);
-    if (newWinner) {
-      setWinner(newWinner);
-    }
+//     // Clean up the WebSocket connection when the component unmounts
+//     return () => {
+//       socket.close();
+//     };
+//   }, []);
 
-    if (socket.readyState === window.WebSocket.OPEN) {
-      socket.send(JSON.stringify({ move: i }));
-    }
-  };
+//   const handleClick = (i) => {
+//     if (board[i] || winner) {
+//       return;
+//     }
 
-  const handleReset = () => {
-    setBoard(initialBoard);
-    setXIsNext(true);
-    setWinner(null);
-  };
+//     const newBoard = board.slice();
+//     newBoard[i] = xIsNext ? 'X' : 'O';
+//     setBoard(newBoard);
+//     setXIsNext(!xIsNext);
 
-  return (
-    <div>
-      <h1>Tic Tac Toe</h1>
-      <div className="game">
-        <TicTacToe
-          board={board}
-          xIsNext={xIsNext}
-          winner={winner}
-          onClick={handleClick}
-        />
-      </div>
-      <button className="reset-btn" onClick={handleReset}>
-        Reset
-      </button>
-    </div>
-  );
-}
+//     const newWinner = calculateWinner(newBoard);
+//     if (newWinner) {
+//       setWinner(newWinner);
+//     }
 
-export default App;
+//     if (socket.connected) {
+//       socket.send({ move: i });
+//     }
+//   };
+
+//   const handleReset = () => {
+//     setBoard(initialBoard);
+//     setXIsNext(true);
+//     setWinner(null);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Tic Tac Toe</h1>
+//       <div className="game">
+//         <TicTacToe
+//           board={board}
+//           xIsNext={xIsNext}
+//           winner={winner}
+//           onClick={handleClick}
+//         />
+//       </div>
+//       <button className="reset-btn" onClick={handleReset}>
+//         Reset
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@
 // import errorMiddleware from './lib/error-middleware.js';
 // import pg from 'pg';
 // import gameRoutes from './routes/gameRoutes.js';
-// import WebSocket from 'ws';
 // import http from 'http';
+// import cors from 'cors';
+// import { Server } from 'socket.io';
 
 // const db = new pg.Pool({
 //   connectionString: process.env.DATABASE_URL,
@@ -11,76 +12,52 @@
 //     rejectUnauthorized: false,
 //   },
 // });
+
 // const app = express();
 
-// const wss = new WebSocket.Server({ server });
+// const server = http.createServer(app);
+// const io = new Server(server); // Create a new Socket.IO server
+
 // console.log('WebSocket server listening on port 8000');
 
 // const games = [];
 
 // // Function to check if the game is over
 // const checkIfGameOver = (board) => {
-//   // Check if there is a winner in the rows.
-//   for (let i = 0; i < 3; i++) {
-//     if (board[i] === board[i + 3] && board[i] === board[i + 6] && board[i]) {
-//       return true;
+//   // ... (rest of the function)
+
+//   io.on('connection', (socket) => {
+//     let game = games.find((g) => g.players.length === 1);
+//     if (!game) {
+//       game = { players: [], board: Array(9).fill(null) };
+//       games.push(game);
 //     }
-//   }
+//     game.players.push(socket);
 
-//   // Check if there is a winner in the columns.
-//   for (let i = 0; i < 3; i++) {
-//     if (board[i * 3] === board[i * 3 + 1] && board[i * 3] === board[i * 3 + 2] && board[i * 3]) {
-//       return true;
-//     }
-//   }
+//     socket.on('message', (message) => {
+//       try {
+//         const move = parseInt(message);
+//         const playerNumber = game.players.indexOf(socket);
+//         game.board[move] = playerNumber;
+//       } catch (error) {
+//         console.error(error);
+//       }
 
-//   // Check if there is a winner in the diagonals.
-//   if (board[0] === board[4] && board[0] === board[8] && board[0]) {
-//     return true;
-//   } else if (board[2] === board[4] && board[2] === board[6] && board[2]) {
-//     return true;
-//   }
+//       const isGameOver = checkIfGameOver(game.board);
 
-//   // Check for a draw
-//   if (!board.includes(null)) {
-//     return true;
-//   }
-
-//   // There is no winner or draw yet
-//   return false;
-// };
-
-// wss.on('connection', (ws) => {
-//   let game = games.find(g => g.players.length === 1);
-//   if (!game) {
-//     game = { players: [], board: Array(9).fill(null) };
-//     games.push(game);
-//   }
-//   game.players.push(ws);
-
-//   ws.on('message', (message) => {
-//     try {
-//       const move = parseInt(message);
-//       const playerNumber = game.players.indexOf(ws);
-//       game.board[move] = playerNumber;
-//     } catch (error) {
-//       console.error(error);
-//     }
-
-//     const isGameOver = checkIfGameOver(game.board);
-
-//     if (isGameOver) {
-//       const returnMessage = JSON.stringify({ isGameOver: true });
-//       game.players.forEach(client => {
-//         client.send(returnMessage);
-//       });
-//     }
+//       if (isGameOver) {
+//         const returnMessage = JSON.stringify({ isGameOver: true });
+//         game.players.forEach((client) => {
+//           client.send(returnMessage);
+//         });
+//       }
+//     });
 //   });
 // });
 
 // app.on('upgrade', (request, socket, head) => {
-//   wss.handleUpgrade(request, socket, head, (ws) => {
-//     wss.emit('connection', ws, request);
+//   io.handleUpgrade(request, socket, head, (socket) => {
+//     io.emit('connection', socket, request);
 //   });
 // });
 
@@ -100,7 +77,6 @@
 
 // app.get('*', (req, res) => res.sendFile(`${reactStaticDir}/index.html`));
 
-// const server = http.createServer(app);
 // server.listen(process.env.PORT, () => {
 //   process.stdout.write(`\n\napp listening on port ${process.env.PORT}\n\n`);
 // });
